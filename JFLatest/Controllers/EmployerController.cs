65e2422cfd978480d5add1db8c5011d9c268fdb6
giza182 <giza182@gmail.com>
2014-10-07 10:10:45 +0200

    added necessary files

---
+++ JFLatest/Controllers/EmployerController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JFLatest.Models;
+
+namespace JFLatest.Controllers
+{
+    public class EmployerController : Controller
+    {
+        private jobfairContext db = new jobfairContext();
+
+        //
+        // GET: /Employer/
+
+        public ActionResult Index()
+        {
+            return View(db.employer.ToList());
+        }
+
+        //
+        // GET: /Employer/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            employer employer = db.employer.Find(id);
+            if (employer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employer);
+        }
+
+        //
+        // GET: /Employer/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employer/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(employer employer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employer.Add(employer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employer);
+        }
+
+        //
+        // GET: /Employer/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            employer employer = db.employer.Find(id);
+            if (employer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employer);
+        }
+
+        //
+        // POST: /Employer/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(employer employer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employer);
+        }
+
+        //
+        // GET: /Employer/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            employer employer = db.employer.Find(id);
+            if (employer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employer);
+        }
+
+        //
+        // POST: /Employer/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            employer employer = db.employer.Find(id);
+            db.employer.Remove(employer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: /AddVacancy
+
+        public ActionResult AddVacancy()
+        {
+            IEnumerable<SelectListItem> booleans = db.boolean.ToList().Select(x => new SelectListItem
+            {
+                Value = x.value.ToString(),
+                Text = x.value
+            });
+
+            AddVacancy addVacancyModel = new AddVacancy();
+
+            addVacancyModel.NoCriminalRecord = booleans;
+            addVacancyModel.Disability = booleans;
+            addVacancyModel.DriversLicense = booleans;
+            addVacancyModel.WorkLocation = db.location.ToList().Select(x => new SelectListItem
+            {
+                Value = x.name.ToString(),
+                Text = x.name
+            });
+
+            addVacancyModel.highestQualification = db.qualification.ToList().Select(x => new SelectListItem
+            {
+                Value = x.description.ToString(),
+                Text = x.description
+            });
+
+
+
+            addVacancyModel.Language = db.language.ToList().Select(x => new SelectListItem
+            {
+                Value = x.name.ToString(),
+                Text = x.name
+            });
+
+
+            addVacancyModel.SalaryRange = db.salary_range.ToList().Select(x => new SelectListItem
+            {
+                Value = x.the_range.ToString(),
+                Text = x.the_range
+            });
+            addVacancyModel.Gender = db.gender.ToList().Select(x => new SelectListItem
+            {
+                Value = x.name.ToString(),
+                Text = x.name
+            });
+            addVacancyModel.Race = db.race.ToList().Select(x => new SelectListItem
+            {
+                Value = x.name.ToString(),
+                Text = x.name
+            });
+
+
+            return View(addVacancyModel);
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JFLatest/Models/EmployerModels.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Web.Mvc;
+namespace JFLatest.Models
+{
+    public class AddVacancy
+    {
+        [Required]
+        public string SelectedItemId { get; set; }
+
+        [Required]
+        [Display(Name = "Language")]
+        public IEnumerable<SelectListItem> Language { get; set; }
+
+
+        [Display(Name = "Clean criminal record")]
+        public IEnumerable<SelectListItem> NoCriminalRecord { get; set; }
+
+
+        [Display(Name = "Driver's license required")]
+        public IEnumerable<SelectListItem> DriversLicense { get; set; }
+
+
+        [Display(Name = "Race")]
+        public IEnumerable<SelectListItem> Race { get; set; }
+
+        [Display(Name = "Work location")]
+        public IEnumerable<SelectListItem> WorkLocation { get; set; }
+
+
+        [Display(Name = "Salary Range")]
+        public IEnumerable<SelectListItem> SalaryRange { get; set; }
+
+        [Display(Name = "Must have disability")]
+        public IEnumerable<SelectListItem> Disability { get; set; }
+
+        [Display(Name = "Gender required for role")]
+        public IEnumerable<SelectListItem> Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Technical Skill 1")]
+        public string technicalSkill1 { get; set; }
+
+        [Display(Name = "Technical Skill 2")]
+        public string technicalSkill2 { get; set; }
+
+        [Display(Name = "Technical Skill 3")]
+        public string technicalSkill3 { get; set; }
+
+        [Required]
+        [Display(Name = "Soft Skill 1")]
+        public string softSkill1 { get; set; }
+
+        [Display(Name = "Soft Skill 2")]
+        public string softSkill2 { get; set; }
+
+        [Display(Name = "Soft Skill 3")]
+        public string softSkill3 { get; set; }
+
+        [Required]
+        [Display(Name = "Highest Qualification")]
+        public IEnumerable<SelectListItem> highestQualification { get; set; }
+
+
+
+
+
+
+        [Required]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "The {0} field must be exactly {2} characters long.")]
+        [Display(Name = "Contact Number")]
+        [RegularExpression("([0][0-9]*)", ErrorMessage = "{0} must contain only numbers and start with 0")]
+        public string contactNumber { get; set; }
+        public string userType { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "The {0} field must be at least {2} characters long.")]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+    }
+}
